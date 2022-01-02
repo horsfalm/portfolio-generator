f4f2d13c59058760f6c56ed1e0f41eaf79ba3a3f
BTV <horsfalm@gmail.com>
2022-01-03 10:57:26 +1100

    update project template

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 const promptUser = () => {
     return inquirer.prompt([
@@ -16,7 +20,7 @@ const promptUser = () => {
         },
         {
             type: 'input',
-            name: 'Github',
+            name: 'github',
             message: 'Enter your GitHub Username (Required)',
             validate: nameInput => {
                 if (nameInput) {
@@ -129,19 +133,15 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+        });
     });
 
 
 
-// const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to the see the output!');
-// });
